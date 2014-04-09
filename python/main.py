da6bc4cfbb1514a5ef96f3e14a3db7cc3318422b
Jintae Jung <jjung@twitter.com>
2014-04-09 16:56:19 -0700

    Working on localizing strings, got hero pics to show

--- python/main.py
+++ python/main.py
@@ -5,8 +5,19 @@ import json
 app = Flask(__name__)
 
 apiKey = '567159D5C2554BBE3419B4F5244C00CF'
+language = 'en_us'
+heroPicSize = "sb.png"
+
 lastFetched = 0
+leaguesFetched = False
 cachedHtml = json.loads('{}')
+
+cachedLeaguesDict = dict()
+cachedLeaguesResp = json.loads('{}')
+
+heroesFetched = False
+cachedHeroesDict = dict()
+cachedHeroesResp = json.loads('{}')
 
 testing = json.loads("""
 					{
@@ -122,14 +133,48 @@ testing = json.loads("""
 	}
 }""")
 
+def get_leagues():
+	global cachedLeaguesResp, leaguesFetched, cachedLeaguesDict
+	if(not leaguesFetched):
+		print("Fetching leagues")
+		leaguesFetched = True
+		response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v0001/?key=' + apiKey + "&language=" + language)
+		cachedLeaguesResp = json.loads(response.read())
+	for league in cachedLeaguesResp['result']['leagues']:
+		cachedLeaguesDict[league['leagueid']] = league['name']
+	return True
+
+def get_heroes():
+	global cachedHeroesResp, heroesFetched, cachedHeroesDict
+	if(not heroesFetched):
+		print("Fetching heroes")
+		heroesFetched = True
+		response = urllib2.urlopen('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=' + apiKey + "&language=" + language)
+		cachedHeroesResp = json.loads(response.read())
+	for hero in cachedHeroesResp['result']['heroes']:
+		cachedHeroesDict[str(hero['id']) + "_name"] = hero['name']
+		cachedHeroesDict[str(hero['id']) + "_localized"] = hero['localized_name']
+	return True
+
 @app.route('/')
 def main_page():
-	global lastFetched, cachedHtml
+	global lastFetched, cachedHtml, cachedHeroesDict, cachedLeaguesDict, heroPicSize
+	get_leagues()
+	get_heroes()
 	now = long(round(time.time()))
-	if now - lastFetched > 1L:
+	if now - lastFetched > 1000000L:
 		lastFetched = now
-		response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/?key=' + apiKey)
+		response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/?key=' + apiKey + "&language=" + language)
 		cachedHtml = json.loads(response.read())
+		for game in testing['result']['games']:
+			game['league_id'] = cachedLeaguesDict[game['league_id']]
+			for player in game['players']:
+				if player['hero_id'] == 0:
+					player['hero_id'] = "Spectator"
+					player['hero_img'] = ""
+				else:
+					player['hero_img'] = "http://cdn.dota2.com/apps/dota2/images/heroes/{}_{}".format(str.replace(str(cachedHeroesDict[str(player['hero_id']) + "_name"]), "npc_dota_hero_", ""), heroPicSize)
+					player['hero_id'] = cachedHeroesDict[str(player['hero_id']) + "_localized"]
 	return render_template('main.html', data=testing)
 
 if __name__ == '__main__':
