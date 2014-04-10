cf682f86875da2f6e5c82712fdd07103ffee0664
Jintae Jung <jjung@twitter.com>
2014-04-10 00:13:20 -0700

    God I'm good at coding (No I'm not; cleaning up shitty code and moving over util methods)

--- python/main.py
+++ python/main.py
@@ -3,25 +3,17 @@ import urllib2
 import urllib
 import time
 import json
-import errno
 import os
+import util
 app = Flask(__name__, static_folder='images', static_url_path='/images')
 
-logoUrl = "http://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/" #?key=APIKEYHERE&appid=570&ugcid="
-apiKey = '567159D5C2554BBE3419B4F5244C00CF'
-language = 'en_us'
+lastFetched = 0
 heroPicSize = "sb.png"
 
-lastFetched = 0
-leaguesFetched = False
-cachedHtml = json.loads('{}')
-
 cachedLeaguesDict = dict()
-cachedLeaguesResp = json.loads('{}')
-
-heroesFetched = False
 cachedHeroesDict = dict()
-cachedHeroesResp = json.loads('{}')
+
+teamDict = {0: 'Radiant', 1: 'Dire', 2: 'Broadcaster', 3: 'Unassigned'}
 
 testing = json.loads("""
 					{
@@ -137,85 +129,45 @@ testing = json.loads("""
 	}
 }""")
 
-def get_leagues():
-	global cachedLeaguesResp, leaguesFetched, cachedLeaguesDict
-	if(not leaguesFetched):
-		print("Fetching leagues")
-		leaguesFetched = True
-		response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v0001/?key=' + apiKey + "&language=" + language)
-		cachedLeaguesResp = json.loads(response.read())
-	for league in cachedLeaguesResp['result']['leagues']:
-		cachedLeaguesDict[league['leagueid']] = league['name']
-	return True
+@app.before_first_request
+def initialize():
+	global cachedLeaguesDict, cachedHeroesDict
+	directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
+	util.require_dir(directory)
+	util.require_dir(os.path.join(directory, "teams"))
 
-def get_heroes():
-	global cachedHeroesResp, heroesFetched, cachedHeroesDict
-	if(not heroesFetched):
-		print("Fetching heroes")
-		heroesFetched = True
-		response = urllib2.urlopen('https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/?key=' + apiKey + "&language=" + language)
-		cachedHeroesResp = json.loads(response.read())
-	for hero in cachedHeroesResp['result']['heroes']:
-		cachedHeroesDict[str(hero['id']) + "_name"] = hero['name']
-		cachedHeroesDict[str(hero['id']) + "_localized"] = hero['localized_name']
-	return True
-
-def convertSteamId(id):
-    if len(id) == 17:
-        return int(id[3:]) - 61197960265728
-    else:
-        return '765' + str(int(id) + 61197960265728)
-
-def require_dir(path):
-    try:
-        os.makedirs(path)
-    except OSError, exc:
-        if exc.errno != errno.EEXIST:
-            raise
-
-def getTeamLogoData(logoId):
-	response = urllib2.urlopen(logoUrl + '?key=' + apiKey + "&appid=570&ugcid=" + str(logoId))
-	return json.loads(response.read())
-
-def getTeamLogo(directory, url, imageName):
-	filename = os.path.join(directory, imageName + ".png")
-
-	if not os.path.exists(filename):
-	    urllib.urlretrieve(url, filename)
+	cachedLeaguesDict = util.get_leagues()
+	cachedHeroesDict = util.get_heroes()
 
 @app.route('/')
 def main_page():
-	global lastFetched, cachedHtml, cachedHeroesDict, cachedLeaguesDict, heroPicSize
-
+	global lastFetched, cachedHtml, heroPicSize, teamDict
 	directory = os.path.join(os.path.dirname(os.path.abspath(__file__)), "images")
-	require_dir(directory)
-	require_dir(os.path.join(directory, "teams"))
 
-	get_leagues()
-	get_heroes()
 	now = long(round(time.time()))
 	if now - lastFetched > 1000000L:
 		lastFetched = now
-		response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/?key=' + apiKey + "&language=" + language)
-		cachedHtml = json.loads(response.read())
+		cachedHtml = util.make_dota2_match_call("GetLiveLeagueGames")
 		for game in testing['result']['games']:
 			
-			game['league_id'] = cachedLeaguesDict[game['league_id']]
+			game['league_name'] = cachedLeaguesDict[game['league_id']]['name']
+			game['tournament_url'] = cachedLeaguesDict[game['league_id']]['tournament_url']
 
 			for player in game['players']:
 				if player['hero_id'] == 0:
 					player['hero_id'] = "Spectator"
 					player['hero_img'] = ""
 				else:
-					player['hero_img'] = "http://cdn.dota2.com/apps/dota2/images/heroes/{}_{}".format(str.replace(str(cachedHeroesDict[str(player['hero_id']) + "_name"]), "npc_dota_hero_", ""), heroPicSize)
+					player['hero_img'] = util.getHeroPicUrl(str(player['hero_id']))
 					player['hero_id'] = cachedHeroesDict[str(player['hero_id']) + "_localized"]
-				player['account_url'] = "http://steamcommunity.com/profiles/{}".format(convertSteamId(str(player["account_id"])))
+				player['account_url'] = util.getPlayerProfileUrl(str(player["account_id"]))
+				player['team'] = teamDict[player['team']]
 
-			radiant_logo_data = getTeamLogoData(game['radiant_team']['team_logo'])
-			dire_logo_data = getTeamLogoData(game['dire_team']['team_logo'])
+			radiant_logo_data = util.getTeamLogoData(game['radiant_team']['team_logo'])
+			dire_logo_data = util.getTeamLogoData(game['dire_team']['team_logo'])
 
-			getTeamLogo(directory, radiant_logo_data['data']['url'], radiant_logo_data['data']['filename'])
-			getTeamLogo(directory, dire_logo_data['data']['url'], dire_logo_data['data']['filename'])
+			util.getTeamLogo(directory, radiant_logo_data['data']['url'], radiant_logo_data['data']['filename'])
+			util.getTeamLogo(directory, dire_logo_data['data']['url'], dire_logo_data['data']['filename'])
 
 			game['radiant_team']['team_logo'] = os.path.join('images', radiant_logo_data['data']['filename'] + '.png')
 			game['dire_team']['team_logo'] = os.path.join('images', dire_logo_data['data']['filename'] + '.png')
---
+++ python/util.py
@@ -0,0 +1,79 @@
+import urllib2
+import urllib
+import json
+import errno
+import os
+
+logoUrl = "http://api.steampowered.com/ISteamRemoteStorage/GetUGCFileDetails/v1/"
+apiKey = '567159D5C2554BBE3419B4F5244C00CF'
+language = 'en_us'
+heroPicSize = "sb.png"
+
+cachedLeaguesDict = dict()
+cachedLeaguesResp = json.loads('{}')
+
+leaguesFetched = False
+cachedHtml = json.loads('{}')
+
+heroesFetched = False
+cachedHeroesDict = dict()
+cachedHeroesResp = json.loads('{}')
+
+def make_econ_dota2_call(callType):
+    response = urllib2.urlopen('https://api.steampowered.com/IEconDOTA2_570/' + callType + '/v0001/?key=' + apiKey + "&language=" + language)
+    return json.loads(response.read())
+
+def make_dota2_match_call(callType):
+    print('https://api.steampowered.com/IDOTA2Match_570/' + callType + '/v0001/?key=' + apiKey + "&language=" + language)
+    response = urllib2.urlopen('https://api.steampowered.com/IDOTA2Match_570/' + callType + '/v0001/?key=' + apiKey + "&language=" + language)
+    return json.loads(response.read())
+
+def get_leagues():
+    global cachedLeaguesResp, leaguesFetched, cachedLeaguesDict
+    if(not leaguesFetched):
+        print("Fetching leagues")
+        leaguesFetched = True
+        cachedLeaguesResp = make_dota2_match_call("GetLeagueListing")
+    for league in cachedLeaguesResp['result']['leagues']:
+        cachedLeaguesDict[league['leagueid']] = league
+    return cachedLeaguesDict
+
+def get_heroes():
+    global cachedHeroesResp, heroesFetched, cachedHeroesDict
+    if(not heroesFetched):
+        print("Fetching heroes")
+        heroesFetched = True
+        cachedHeroesResp = make_econ_dota2_call("GetHeroes")
+    for hero in cachedHeroesResp['result']['heroes']:
+        cachedHeroesDict[str(hero['id']) + "_name"] = hero['name']
+        cachedHeroesDict[str(hero['id']) + "_localized"] = hero['localized_name']
+    return cachedHeroesDict
+
+def convertSteamId(id):
+    if len(id) == 17:
+        return int(id[3:]) - 61197960265728
+    else:
+        return '765' + str(int(id) + 61197960265728)
+
+def require_dir(path):
+    try:
+        os.makedirs(path)
+    except OSError, exc:
+        if exc.errno != errno.EEXIST:
+            raise
+
+def getTeamLogoData(logoId):
+	response = urllib2.urlopen(logoUrl + '?key=' + apiKey + "&appid=570&ugcid=" + str(logoId))
+	return json.loads(response.read())
+
+def getTeamLogo(directory, url, imageName):
+	filename = os.path.join(directory, imageName + ".png")
+
+	if not os.path.exists(filename):
+	    urllib.urlretrieve(url, filename)
+
+def getHeroPicUrl(heroId):
+    return "http://cdn.dota2.com/apps/dota2/images/heroes/{}_{}".format(str.replace(str(cachedHeroesDict[heroId + "_name"]), "npc_dota_hero_", ""), heroPicSize)
+
+def getPlayerProfileUrl(playerId):
+    return "http://steamcommunity.com/profiles/{}".format(convertSteamId(playerId))
